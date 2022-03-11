194fde136f7816c50cbc87689b3c9ac86fc28ed7
Luiz Camargo <luiz.zito@hotmail.com>
2022-03-11 18:55:57 -0300

    formatação arquivos

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,17 +1,17 @@
-function toggleMenu () {
-   tratarMenu();
-   tratarLinhas();
+function toggleMenu() {
+    tratarMenu();
+    tratarLinhas();
 }
 
-function tratarMenu () {
+function tratarMenu() {
     const menu = document.getElementById('menu');
     menu.classList.toggle('active');
 }
 
-function tratarLinhas () {
-    const linhas = document.getElementsByClassName('linha');  
+function tratarLinhas() {
+    const linhas = document.getElementsByClassName('linha');
     for (let index = 0; index < linhas.length; index++) {
-        linhas[index].classList.toggle('active');    
+        linhas[index].classList.toggle('active');
     }
 }
 
